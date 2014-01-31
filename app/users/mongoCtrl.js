02d8b96797f4c0af03d1966cc2ade8bbbe077cbe
Tomasz Wszelaki <tomekwszelaki@gmail.com>
2014-01-31 13:17:53 +0100

    Move getConnection execution to mongoCtrl - now the whole mongodb connection establishing/renewal is located in mongoCtrl module. Expose a unique endpoint for mongo queries (execute method) instead of exposing each method separately - done to centralize db connection establishment and management. Refactor users api methods (style changes mostly).

--- app/users/mongoCtrl.js
+++ app/users/mongoCtrl.js
@@ -1,15 +1,33 @@
 /**
  * Created by tomasj on 28/01/14.
  */
+
+var collections = {accounts: "accounts"}
+var methods = {insert: insert, save: save, findOne: findOne, find: find}
+
 var log = require("winston").loggers.get("app:mongoCtrl");
 var mongoClient = require('mongodb').MongoClient;
 var async = require('async');
 var conn = null;
 
+function execute(method, col, doc, cb) {
+    getConnection(function(err){
+        if (err) cb(err, null);
+        switch(method) {
+            case methods.find: find(col, doc, cb); break;
+            case methods.findOne: findOne(col, doc, cb); break;
+            case methods.insert: insert(col, doc, cb); break;
+            case methods.save: save(col, doc, cb); break;
+        }
+    })
+}
+
+// I'm not sure whether getConnection should be called by each of the mongoCtrl method internally or should it be called
+// by other modules before calling the actual mongoCtrl method like insert or find
 function getConnection(callback) {
     if (conn) {
         log.info("Connection established previously, returning conn")
-        return callback(null, conn);
+        return callback(null);
     }
     else {
        async.whilst(
@@ -22,30 +40,70 @@ function getConnection(callback) {
                 if (err) throw err;
                 conn = db;
                 log.info("db connection established");
-                return callback(null, conn);
+                return callback(null);
             })
             setTimeout(call, 1000);
         },
         function(err) {
-            if (err) throw err;
+            if (err) return callback(err);
         });
     }
 }
 
-function insert(conn, col, doc) {
-    log.debug("going to insert: " + doc + " to " + col);
+function insert(col, doc, cb) {
+    log.debug("going to upsert: " + doc + " to " + col);
     var collection = conn.collection(col);
     collection.insert(doc, function(err, docs) {
-        if (err) log.error(err);
-        log.debug("insert successfull");
+        if (err) cb(err, null);
+        log.info("collection:", col, "insert successful");
+        log.debug("document created/modified: ", JSON.stringify(docs, null, 4));
+        cb(null, docs); // return a cursor object
     });
 };
 
-function findOne(conn, col, doc) {
+function save(col, doc, cb) {
+    log.debug("going to save: ", doc, " to ", col);
     var collection = conn.collection(col);
-    collection.findOne(doc, console.log);
+    collection.save(doc, {safe: true}, function(err, docs) {
+        if (err) cb(err, null);
+        log.info("collection:", col, "save successful");
+        log.debug("document created/modified: ", JSON.stringify(docs, null, 4));
+        cb(null, docs); // return an int - the number of documents changed
+    });
+};
+
+function findOne(col, doc, cb) {
+    log.debug("going to findOne: ", doc, " in ", col);
+    var collection = conn.collection(col);
+    collection.findOne(doc, function(err, docs) {
+        if (err) cb(err, null);
+        log.info("collection: ", col, "findOne successful");
+        log.debug("record found: ", JSON.stringify(docs, null, 4));
+        cb(null, docs); // returns a single document
+    });
 }
 
-exports.getConnection = getConnection;
-exports.insert = insert;
-exports.findOne = findOne
+function find(col, doc, cb) {
+    log.debug("going to find: ", doc, " in ", col);
+    var collection = conn.collection(col);
+    collection.find(doc, function(err, docs) {
+        if (err) cb(err, null);
+        log.info("collection: ", col, "find successful");
+        // docs - is a cursor object, we have to convert it into an Array to be able to return it
+        // WARNING / TODO: paging is necesasry as "toArray" loads the whole collection into memory
+        docs.toArray(function(err, data) {
+            if (err) cb(err, null);
+            log.debug("record(s) found: ", data.length);
+            cb(null, data);
+        })
+    });
+}
+
+//exports.getConnection = getConnection;
+//exports.insert = insert;
+//exports.save = save;
+//exports.findOne = findOne;
+//exports.find = find;
+exports.execute = execute;
+exports.collections = collections;
+exports.methods = methods;
--- app/users/routes.js
+++ app/users/routes.js
@@ -4,45 +4,76 @@
 
 var log = require("winston").loggers.get("app:users");
 var mongo = require('./mongoCtrl');
+var ObjectId = require('mongodb').ObjectID;
 var express = require('express');
 
 function createUser(req, res) {
-    log.debug("createUser request: " + req.body)
-    var doc = {}
-    if ("email" in req.body){
-        doc.email = req.body.email;
-    }
-    else {
-        res.send(400, "Please specify an email");
-    }
-    if ("nick" in req.body){
-        doc.nick = req.body.nick;
-    }
-    if ("details" in req.body){
-        doc.details = req.body.details;
-    }
-    if ("friends" in req.body){
-        doc.friends = req.body.friends;
-    }
-    doc.photoId = "newObjectID";
-    console.log("doc: " + JSON.stringify(doc, null, 4));
-    mongo.getConnection(function(err, conn) {
-        if(err) throw err;
-        mongo.insert(conn, "accounts", doc);
-        res.send(201, "User created successfully");
+    log.debug("createUser request: ", req.body);
+    var doc = {};
+    doc.email = "email" in req.body ? req.body.email : res.send(400, "Please specify an email");
+    doc.nick = "nick" in req.body ? req.body.nick : "";
+    doc.details = "details" in req.body ? req.body.details : {name: "", surname: "", gender: "", age: ""};
+    doc.friends = "friends" in req.body ? req.body.friends : {};
+    doc.photoId = ObjectId("newObjectId1");
+    mongo.execute(mongo.methods.insert, mongo.collections.accounts, doc, function(err, data) {
+        if (err) throw err;
+        res.set("Content-Type", "application/json");
+        // as insert returns a cursor object (an array more or less) so we have to user data[0] to access the result
+        res.send(201, {id: data[0]._id, email: data[0].email});
+    });
+}
+
+// should be internal - not even sure it should exist :P
+function getUsers(req, res) {
+    log.debug("getUsers request: ", req.params);
+    mongo.execute(mongo.methods.find, mongo.collections.accounts, {}, function(err, data){
+        if (err) throw err;
+        res.set("Content-Type", "application/json");
+        res.send(200, data);
     })
 }
 
-function getUsers(req, res) {
-    if ("email" in req.query) {
-        res.send("searching for: " + req.query.email)
-    }
-    res.send("this is the right place");
+function getUser(req, res) {
+    log.debug("getUser request: ", req.params);
+    var doc = {_id: ObjectId(req.params.id)}
+    mongo.execute(mongo.methods.findOne, mongo.collections.accounts, doc, function(err, data) {
+        if (err) throw err;
+        res.set("Content-Type", "application/json");
+        res.send(200, data);
+    });
+}
+
+function getUserFriends(req, res) {
+    log.debug("getUserFriends request: ", req.params);
+    var doc = {_id: ObjectId(req.params.id)}
+    mongo.execute(mongo.methods.findOne, mongo.collections.accounts, doc, function(err, data) {
+        if (err) throw err;
+        res.set("Content-Type", "application/json");
+        res.send(200, {friends: data.friends});
+    });
+}
+
+function modifyUser(req, res) {
+    log.debug("modifyUser request: ", req.body)
+    var doc = {_id: ObjectId(req.params.id)};
+    if ("email" in req.body) doc.email =  req.body.email;
+    if ("nick" in req.body) doc.nick =  req.body.nick;
+    if ("details" in req.body) doc.details =  req.body.details;
+    if ("friends" in req.body) doc.friends =  req.body.friends;
+    mongo.execute(mongo.methods.save, mongo.collections.accounts, doc, function(err, data) {
+        if (err) throw err;
+        res.set("Content-Type", "application/json");
+        if (data === 1) res.send(204, doc);
+        else res.send(204, data);
+    });
 }
 
 function setup(app) {
     app.post('/api/users', express.bodyParser(), createUser);
     app.get('/api/users', getUsers);
+    app.get('/api/users/:id', getUser);
+    app.get('/api/users/:id/friends', getUserFriends);
+    app.put('/api/users/:id', express.bodyParser(), modifyUser);
 }
 
 module.exports = setup
