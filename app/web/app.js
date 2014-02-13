171e1f44c9ea9a576cee84ec4153c3a38711448a
Gerardo Jimenez Tornos <gerardooscarjt@gmail.com>
2014-02-13 09:10:59 +0100

    Add getProjectedDimension Fix drawZombie Remove dead code Restore original room background

--- app/web/app.js
+++ app/web/app.js
@@ -9,6 +9,9 @@ var game = null;
 window.addEventListener('load', function(e){
 	var canvas = document.getElementById('zombie-canvas');
 	game = new ZombieWarGame(canvas);
+
+	game.perspective = {h: 302, m: 713, o: 20};
+
 	setInterval(function(){
 		game.tick();	
 	}, 33);
@@ -26,6 +29,7 @@ window.addEventListener('load', function(e){
 }, true);
 
 
+//////////////////////////////////////////////////////////// ImageManager
 function ImageManager() {
 
 }
@@ -43,19 +47,19 @@ ImageManager.get = function(url) {
 
 /////////////////////////////////////////////////////////// Zombie
 function Zombie() {
-
 	this.image = ImageManager.get('/img/zombie.png');
-	this.width = 200;
-	this.height = 400;
 	this.position = {
-		x: 300,
-		y: 100,
+		x: -350,
+		y: 1000,
 		z: 0
-	}
-
+	};
+	this.dimension = {
+		x: 200,
+		y: 0,
+		z: 400
+	};
 }
 // TODO: Zombie.goUp, Zombie.goDown, Zombie.goLeft, Zombie.goRight
-
 
 /////////////////////////////////////////////////////////// Point
 function Point() {
@@ -65,7 +69,6 @@ function Point() {
 		z: 0
 	}
 }
-
 
 /////////////////////////////////////////////////////////// ZombieWarGame
 function ZombieWarGame(canvas) {
@@ -85,46 +88,25 @@ function ZombieWarGame(canvas) {
 
 	this.refreshContext();
 
-	// Control zombie movements:
-	// window.addEventListener('keydown', function(e) {
-	// 	var kc = e.keyCode;
-	// 	switch (kc) {
-	// 		case 37: // Left
-	// 			zombie.position.x -= 10;
-	// 			break;
-	// 		case 39: // Right
-	// 			zombie.position.x += 10;
-	// 			break;
-	// 		case 38: // Up
-	// 			zombie.position.y -= -3;
-	// 			zombie.position.x += 3;
-	// 			zombie.width -= 6;
-	// 			zombie.height -= 14;
-	// 			break;
-	// 		case 40: // Down
-	// 			zombie.position.y += -3;
-	// 			zombie.position.x -= 3;
-	// 			zombie.width += 6;
-	// 			zombie.height += 14;
-	// 			break;
-	// 	}
-	// }, true);
-
 	window.addEventListener('keydown', function(e) {
 		var kc = e.keyCode;
 		console.log(kc);
 		switch (kc) {
 			case 37: // Left
 				point.position.x -= 10;
+				zombie.position.x -= 10;
 				break;
 			case 39: // Right
 				point.position.x += 10;
+				zombie.position.x += 10;
 				break;
 			case 38: // Up
 				point.position.y += 10;
+				zombie.position.y += 10;
 				break;
 			case 40: // Down
 				point.position.y -= 10;
+				zombie.position.y -= 10;
 				break;
 			case 49: // 1
 				perspective.h += 1;
@@ -145,6 +127,8 @@ function ZombieWarGame(canvas) {
 				perspective.o -= 1;
 				break;
 		}
+
+		console.log(zombie.position);
 	}, true);
 
 }
@@ -157,9 +141,9 @@ ZombieWarGame.prototype.tick = function() {
 	this.ticks++;
 
 	this.drawBackground();
-//	this.drawZombie();
-	this.drawPoint();
-	this.drawPerspectiveLines();
+	this.drawZombie();
+	// this.drawPoint();
+	// this.drawPerspectiveLines();
 };
 
 ZombieWarGame.prototype.refreshContext = function() {
@@ -175,12 +159,19 @@ ZombieWarGame.prototype.drawBackground = function() {
 };
 
 ZombieWarGame.prototype.drawZombie = function() {
+
+	var zombie = this.zombie;
+
+	var position = this.getProjectedPoint(zombie.position);
+
+	var dimension = this.getProjectedDimension(zombie.position, zombie.dimension);
+
 	this.ctx.drawImage(
-		this.zombie.image,
-		this.zombie.position.x,
-		this.zombie.position.y,
-		this.zombie.width,
-		this.zombie.height
+		zombie.image,
+		position.x,
+		position.y - dimension.z,
+		dimension.x,
+		dimension.z
 	);
 };
 
@@ -193,6 +184,25 @@ ZombieWarGame.prototype.drawPoint = function() {
 	this.ctx.lineWidth = 1;
 	this.ctx.strokeStyle = '#00FF00';
 	this.ctx.stroke();
+};
+
+ZombieWarGame.prototype.getProjectedDimension = function(point, dimension) {
+	var	y = point.y,
+		x = point.x,
+		ph = this.perspective.h,
+		pm = this.perspective.m,
+		po = this.perspective.o,
+		height = this.canvas.height,
+		width = this.canvas.width;
+
+	var coefficient = (1-y/(y+pm));
+	var constant = 0;
+
+	return {
+		x: constant + dimension.x*coefficient,
+		y: 0,
+		z: constant + dimension.z*coefficient,
+	};
 };
 
 ZombieWarGame.prototype.getProjectedPoint = function(point) {
@@ -209,44 +219,50 @@ ZombieWarGame.prototype.getProjectedPoint = function(point) {
 		x: po+width/2+x*(1-partial),
 		y: height - ph*partial,
 		z: 0,
-	}
+	};
 };
 
-ZombieWarGame.prototype.drawPerspectiveLines = function() {
-
+ZombieWarGame.prototype.drawPerspectiveFloor = function() {
 	// Draw lines to infinite
 	this.ctx.lineWidth = 1;
-	this.ctx.strokeStyle = 'rgba(0,255,0,0.5)';
+	this.ctx.strokeStyle = '#00FF00';
 
 	this.ctx.beginPath();
 	var n = 100;
 	var l = 2000;
 	for (var i=-n; i<=n; i++) {
 		var from = this.getProjectedPoint({x:20*i, y:0, z:0});
-		var to = this.getProjectedPoint({x:20*i, y:l, z:0})
+		var to = this.getProjectedPoint({x:20*i, y:l, z:0});
 		this.ctx.moveTo(from.x, from.y);
 		this.ctx.lineTo(to.x, to.y);
 	}
 	this.ctx.stroke();
+};
 
-
+ZombieWarGame.prototype.drawPerspectiveHorizon = function() {
 	// Draw horizon line
 	this.ctx.lineWidth = 1;
-	this.ctx.strokeStyle = 'rgba(255,0,0,1)';
+	this.ctx.strokeStyle = '#FF0000';
 	this.ctx.beginPath();
 	this.ctx.moveTo(0, this.canvas.height-this.perspective.h);
 	this.ctx.lineTo(this.canvas.width, this.canvas.height-this.perspective.h);
 	this.ctx.stroke();
+};
 
+ZombieWarGame.prototype.drawPerspectiveMiddle = function() {
 	// Draw mid line
 	this.ctx.lineWidth = 1;
-	this.ctx.strokeStyle = 'rgba(255,0,0,1)';
+	this.ctx.strokeStyle = '#FF0000';
 	this.ctx.beginPath();
 	this.ctx.moveTo(this.perspective.o + this.canvas.width/2, this.canvas.height-this.perspective.h);
 	this.ctx.lineTo(this.perspective.o + this.canvas.width/2, this.canvas.height);
 	this.ctx.stroke();
+};
 
-
+ZombieWarGame.prototype.drawPerspectiveLines = function() {
+	this.drawPerspectiveFloor();
+	this.drawPerspectiveHorizon();
+	this.drawPerspectiveMiddle();
 };
 
 ZombieWarGame.prototype.drawSquare = function() {
